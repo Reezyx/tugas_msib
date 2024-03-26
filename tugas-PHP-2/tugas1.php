fb83fe685ce5a0030c0fff59e2e0c6e6a59b4dcb
Fathin Fayyadh <fathinfayyadh4@gmail.com>
2024-03-27 04:48:11 +0700

    Tugas PHP

--- tugas-PHP-2/tugas1.php
+++ tugas-PHP-2/tugas1.php
@@ -5,6 +5,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Print Table</title>
+    <style>
+        body {
+            display: grid;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 500px;
+            border: solid;
+        }
+
+        th,
+        td {
+            padding: 8px;
+            text-align: left;
+            border: solid;
+        }
+
+        tr.table-menu {
+            border: black;
+            background-color: aquamarine;
+        }
+
+        
+        tr:not(.table-menu):hover {
+            background-color: grey; 
+            cursor: pointer;
+        }
+
+        tr.hover-even {
+            background-color: #ddd; 
+        }
+
+        tr.hover-odd {
+            background-color: #fff; 
+        }
+    </style>
+
 </head>
 
 <body>
@@ -30,9 +71,28 @@
             "nama_makanan" => "Bakso",
             "harga" => 13000
         ],
-    ]
-    ?>
+    ];
 
+    ?>
+    <h1>List Harga Makanan</h1>
+    <table>
+        <tr class="table-menu">
+            <th>No</th>
+            <th>Nama Makanan</th>
+            <th>Harga</th>
+        </tr>
+        <?php
+        $no = 1;
+        foreach ($lists as $list) {
+            $row_class = ($no % 2 == 0) ? "hover-even" : "hover-odd";
+        ?>
+            <tr class="table-row <?php echo $row_class; ?>">
+                <td><?php echo $no++; ?></td>
+                <td><?php echo $list['nama_makanan']; ?></td>
+                <td><?php echo "Rp " . number_format($list['harga'], 0, ',', '.'); ?></td>
+            </tr>
+        <?php } ?>
+    </table>
 
 </body>
 
--- tugas-PHP-2/tugas2_post.php
+++ tugas-PHP-2/tugas2_post.php
@@ -5,22 +5,94 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nilai Mahasiswa</title>
+    <style>
+        .container-card{
+            display:grid;
+            justify-content: center;
+         align-items: center;
+         margin-top: 40px;
+        }
+        .card{
+            width: 500px;
+            border: solid 1px;
+            padding: 30px;
+        }
+      .wrapper{
+    display: flex;
+    justify-content: space-between;
+}
+.info{
+    text-align: center;
+}
+    .message-success {
+         color: blue; 
+        }
+           .message-fail {
+            color: red; 
+        }
+    </style>
 
 </head>
 
 <body>
     <?php
-    /* 
-        Kondisi Nilai Yang dikeluarkan
-        >= 80          : Selamat kamu lulus dengan nilai A (Tulisan Biru)
-        < 80 dan >= 70 : Selamat kamu lulus dengan nilai B (Tulisan Biru)
-        < 70 dan >= 60 : Selamat kamu lulus dengan nilai C (Tulisan Biru)
-        < 60 dan >= 50 : Kamu gagal dengan nilai D (Tulisan Merah)
-        < 50           : Kamu gagal dengan nilai E (Tulisan Merah)
 
-    */
+   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+       $nama = $_POST["nama"];
+       $username = $_POST["username"];
+       $nim = $_POST["nim"];
+       $nilai = $_POST["nilai"];
+   
+       $grade = "";
+       if ($nilai >= 80) {
+           $grade = "A";
+       } elseif ($nilai >= 70) {
+           $grade = "B";
+       } elseif ($nilai >= 60) {
+           $grade = "C";
+       } elseif ($nilai >= 50) {
+           $grade = "D";
+       } else {
+           $grade = "E";
+       }
+   
+       $message = "";
+
+    if ($grade === "A" || $grade === "B" || $grade === "C") {
+        // Pesan lulus
+        $message = "Selamat, Anda lulus Dengan Nilai  ";
+        $message_class = "message-success";
+    } else {
+        // Pesan tidak lulus
+        $message = "Maaf, Anda tidak lulus Dengan Nilai ";
+        $message_class = "message-fail";    
+    }
+   }
     ?>
-    
+    <div class="container-card">
+        <div class="card">
+                <h2 style="text-align: center;">Data Mahasiswa</h3>
+                  <div class="wrapper">
+                <h4>Nama </h4>
+                <h4><?php echo $nama;?></h4>
+              </div>
+              <div class="wrapper">
+                <h4>Username</h4>
+                <h4><?php echo $username;?></h4>
+              </div>
+              <div class="wrapper">
+                <h4>NIM</h4>
+                <h4><?php echo  $nim;?></h4>
+              </div>
+              <div class="wrapper">
+                <h4>Score</h4>
+                <h4><?php echo  $nilai;?></h4>
+              </div>
+              <div class="info">
+                <h3 class="<?php echo $message_class; ?>"><?php echo $message , $grade;?></h3>          
+              </div>
+            </div>
+    </div>
 </body>
 
 </html>
