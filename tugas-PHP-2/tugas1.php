864f38db410176597751c4ca602ea47579c7fc31
Farhan Abdul Hamid <arhandev.code@gmail.com>
2024-03-26 13:22:09 +0700

    add tugas 2 php

---
+++ tugas-PHP-2/tugas1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print Table</title>
+</head>
+
+<body>
+    <?php
+    $lists = [
+        [
+            "nama_makanan" => "Sate Ayam",
+            "harga" => 5000
+        ],
+        [
+            "nama_makanan" => "Sate Padang",
+            "harga" => 15000
+        ],
+        [
+            "nama_makanan" => "Nasi Padang",
+            "harga" => 25000
+        ],
+        [
+            "nama_makanan" => "Sate Kambing",
+            "harga" => 20000
+        ],
+        [
+            "nama_makanan" => "Bakso",
+            "harga" => 13000
+        ],
+    ]
+    ?>
+
+
+</body>
+
+</html>
---
+++ tugas-PHP-2/tugas2_post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nilai Mahasiswa</title>
+
+</head>
+
+<body>
+    <?php
+    /* 
+        Kondisi Nilai Yang dikeluarkan
+        >= 80          : Selamat kamu lulus dengan nilai A (Tulisan Biru)
+        < 80 dan >= 70 : Selamat kamu lulus dengan nilai B (Tulisan Biru)
+        < 70 dan >= 60 : Selamat kamu lulus dengan nilai C (Tulisan Biru)
+        < 60 dan >= 50 : Kamu gagal dengan nilai D (Tulisan Merah)
+        < 50           : Kamu gagal dengan nilai E (Tulisan Merah)
+
+    */
+    ?>
+    
+</body>
+
+</html>
